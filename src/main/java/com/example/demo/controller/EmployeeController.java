c040376867dedbda244792b10dff165d5c1fa839
dengzhiwen <dengzhiwen@kuaishou.com>
2020-07-03 10:52:06 +0800

    实现老人、义工、员工的批量图片的接收，可以保存图片路径

--- src/main/java/com/example/demo/controller/EmployeeController.java
+++ src/main/java/com/example/demo/controller/EmployeeController.java
@@ -2,13 +2,17 @@ package com.example.demo.controller;
 
 import com.example.demo.bo.DeleteByIdBo;
 import com.example.demo.bo.LoginBo;
+import com.example.demo.bo.PicturesBo;
 import com.example.demo.entry.Employee;
 import com.example.demo.service.EmployeeService;
+import com.example.demo.service.PicturesService;
 import com.example.demo.util.ResultReturn;
 import com.example.demo.util.ResultReturnUtil;
 import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestHeader;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.multipart.MultipartFile;
 
 import javax.annotation.Resource;
 
@@ -25,6 +29,9 @@ public class EmployeeController {
 
     @Resource
     private EmployeeService employeeService;
+
+    @Resource
+    private PicturesService picturesService;
 
     @RequestMapping("login")
     public ResultReturn login(@RequestBody LoginBo loginBo){
@@ -49,6 +56,17 @@ public class EmployeeController {
     @RequestMapping("/getAll")
     public ResultReturn getAll(){
         return employeeService.getAll();
+    }
+
+    @RequestMapping("/pictures")
+    public ResultReturn submitPictures(@RequestBody MultipartFile[] pictures, @RequestHeader("token") String token){
+        PicturesBo picturesBo = new PicturesBo();
+        picturesBo.setPictures(pictures);
+        picturesBo.setToken(token);
+        picturesBo.setType("employee");
+
+        System.out.println(picturesBo.toString());
+        return picturesService.savePictures(picturesBo);
     }
 
 }
--- src/main/java/com/example/demo/controller/VolunteerController.java
+++ src/main/java/com/example/demo/controller/VolunteerController.java
@@ -2,12 +2,16 @@ package com.example.demo.controller;
 
 import com.example.demo.bo.DeleteByIdBo;
 import com.example.demo.bo.LoginBo;
+import com.example.demo.bo.PicturesBo;
 import com.example.demo.entry.Volunteer;
+import com.example.demo.service.PicturesService;
 import com.example.demo.service.VolunteerService;
 import com.example.demo.util.ResultReturn;
 import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestHeader;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.multipart.MultipartFile;
 import sun.jvm.hotspot.debugger.ReadResult;
 
 import javax.annotation.Resource;
@@ -22,6 +26,9 @@ public class VolunteerController {
 
     @Resource
     private VolunteerService volunteerService;
+
+    @Resource
+    private PicturesService picturesService;
 
     @RequestMapping("login")
     public ResultReturn login(@RequestBody LoginBo loginBo){
@@ -46,5 +53,16 @@ public class VolunteerController {
     @RequestMapping("/getAll")
     public ResultReturn getAll(){
         return volunteerService.getAll();
+    }
+
+    @RequestMapping("/pictures")
+    public ResultReturn submitPictures(@RequestBody MultipartFile[] pictures, @RequestHeader("token") String token){
+        PicturesBo picturesBo = new PicturesBo();
+        picturesBo.setPictures(pictures);
+        picturesBo.setToken(token);
+        picturesBo.setType("volunteer");
+
+        System.out.println(picturesBo.toString());
+        return picturesService.savePictures(picturesBo);
     }
 }
