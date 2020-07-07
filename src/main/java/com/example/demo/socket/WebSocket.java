351d5a06af53cac21f743a0bf9f09363801e2bf5
dengzhiwen <dengzhiwen@kuaishou.com>
2020-07-07 14:32:54 +0800

    使用LogBack来替代sout打印输出

--- src/main/java/com/example/demo/socket/WebSocket.java
+++ src/main/java/com/example/demo/socket/WebSocket.java
@@ -3,6 +3,8 @@ package com.example.demo.socket;
 import ch.qos.logback.core.encoder.EchoEncoder;
 import com.example.demo.mapper.EventMapper;
 import com.example.demo.util.EncoderClassVo;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.context.annotation.Bean;
 import org.springframework.stereotype.Component;
 import org.springframework.web.socket.server.standard.ServerEndpointExporter;
@@ -23,6 +25,8 @@ import java.util.concurrent.CopyOnWriteArraySet;
 @ServerEndpoint(value = "/ws/webSocket", encoders = {EncoderClassVo.class})
 public class WebSocket {
 
+    private Logger logger = LoggerFactory.getLogger(WebSocket.class);
+
     private Session session;
     private static CopyOnWriteArraySet<WebSocket> copyOnWriteArraySet = new CopyOnWriteArraySet<>();
 
@@ -30,29 +34,30 @@ public class WebSocket {
     public void onOpen(Session session){
         this.session = session;
         copyOnWriteArraySet.add(this);
-        System.out.println("websocket有新的连接, 总数:"+ copyOnWriteArraySet.size());
+        logger.info("websocket有新的连接, 总数:"+ copyOnWriteArraySet.size());
     }
 
     @OnClose
     public void onClose(){
         copyOnWriteArraySet.remove(this);
-        System.out.println("websocket连接断开, 总数:"+ copyOnWriteArraySet.size());
+        logger.info("websocket连接断开, 总数:"+ copyOnWriteArraySet.size());
     }
 
     @OnMessage
     public void onMessage(String message){
-        System.out.println("websocket收到客户端发来的消息:"+message);
+        logger.info("websocket收到客户端发来的消息:"+message);
     }
 
     @OnError
     public void onError(Session session, Throwable error) {
-        System.out.println("发生错误：" + error.getMessage() + "; sessionId:" + session.getId());
+        logger.info("发生错误：" + error.getMessage() + "; sessionId:" + session.getId());
         error.printStackTrace();
     }
     
     public void sendMessage(Object object){
         for (WebSocket webSocket : copyOnWriteArraySet) {
-            System.out.println("websocket广播消息：" + object.toString());
+
+            logger.info("websocket广播消息：" + object.toString());
             try {
                 webSocket.session.getBasicRemote().sendObject(object);
             }catch (Exception e){
@@ -63,7 +68,7 @@ public class WebSocket {
 
     public void sendMessage(String message){
         for (WebSocket webSocket : copyOnWriteArraySet) {
-            System.out.println("websocket广播消息：" + message);
+            logger.info("websocket广播消息：" + message);
             try {
                 webSocket.session.getBasicRemote().sendText(message);
             }catch (Exception e){
